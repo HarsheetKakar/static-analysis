0cdd8f47bc205643df6390af8af643e8fb568009
Harsheet Kakar <harsheetkakar@gmail.com>
2021-05-02 13:21:02 +0530

    abstract tree class made

--- static_analyzer/utils/ast/abstract_tree.py
+++ static_analyzer/utils/ast/abstract_tree.py
@@ -4,13 +4,24 @@ from ast import parse
 from ast2json import ast2json
 
 
-def construct(filename):
-    with open(filename, "r") as f:
-        code = f.read()
-        abstract_tree = parse(code)
-    return ast2json(abstract_tree)
+class AST_maker:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.__ast = None
+
+    @property
+    def abstract_tree(self):
+        return self.__ast
+
+    def construct(self):
+        with open(self.filepath, "r") as f:
+            code = f.read()
+            abstract_tree = parse(code)
+        self.__ast = ast2json(abstract_tree)
+        return self
 
 
 if __name__ == '__main__':
-    print(construct(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "ast_test.py")))
+    ast = AST_maker(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "ast_test.py")).construct()
+    console.log(abstract_tree)
